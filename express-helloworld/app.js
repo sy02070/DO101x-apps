99b22ff635a28929c2d80baa7d48509a77caf169
sy02070 <sylexuan72@gmail.com>
2025-07-15 10:32:34 +0700

    .

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World! ver.2\n');
 });
 
 app.get('/mars', function(req, res) { res.send('Hello Mars!\n'); });
